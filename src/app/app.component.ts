53215d26bad1a759da7d5c732d81eaa5fa3213d1
shailendra <ssbaghel.95@gmail.com>
2019-11-30 16:51:45 +0530

    i m client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularApp';
+
+  constructor(private service:AuthService,private router:Router){}
+Logout ()
+{
+  this.service.Logout();
+  this.router.navigate(['login']);
 }
+}
+
